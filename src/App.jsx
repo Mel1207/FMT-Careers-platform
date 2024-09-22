964907e2b0c27769710a0b746d6e7fa8a3c56d05
Mel <57514727+Mel1207@users.noreply.github.com>
2024-09-23 07:20:28 +0800

    added admin and fix navbar actives

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,23 @@ import PageCareers from "./pages/PageCareers"
 import PageHome from "./pages/PageHome"
 import PageAbout from "./pages/PageAbout"
 import PageAdmin from "./pages/PageAdmin"
+import NavbarAdmin from "./components/NavbarAdmin"
 
 function App() {
   const location = useLocation()
   console.log(location.pathname)
   return (
     <>
-      {location.pathname !== '/admin/5b1' ? (
-        <>
-          {/* CAREERS INTERFACE */}
-          <Navbar /> 
-          <div className="app">
-            <Routes>
-              <Route path="/" exact element={<PageHome />} />
-              <Route path="/about" element={<PageAbout />} />
-              <Route path="/careers" element={<PageCareers />}/>
-            </Routes>
-          </div>
-        </>
-      ) : (
-        <>
-          {/* ADMIN INTERFACE */}
-          <div className="app">
-            <Routes>
-              <Route path="/admin/5b1" element={<PageAdmin />}/>
-            </Routes>
-          </div>
-        </>
-      )}
+      {location.pathname !== '/admin' ? <Navbar /> : <NavbarAdmin />}
+      
+      <div className="app">
+        <Routes>
+          <Route path="/" exact element={<PageHome />} />
+          <Route path="/about" element={<PageAbout />} />
+          <Route path="/careers" element={<PageCareers />} />
+          <Route path="/admin" element={<PageAdmin />} />
+        </Routes>
+      </div>
     </>
   )
 }
--- src/pages/PageAdmin.jsx
+++ src/pages/PageAdmin.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
 
 const PageAdmin = () => {
   return (
-    <div>PageAdmin</div>
+    <>
+      <Sidebar />
+      <div className='container'>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos repellat distinctio facere ullam, voluptatibus ut, labore saepe autem consequatur illo totam, tempore animi voluptatem officiis nisi similique ducimus repellendus perferendis quod est! Hic, laudantium modi? Officiis veniam earum, natus adipisci laborum, voluptas modi voluptatum corrupti eius blanditiis itaque est dolore placeat neque. Doloremque fuga amet molestiae dolore mollitia, laborum autem? Tenetur voluptatibus a possimus eius repudiandae, aliquid fugit quasi atque dolores minus consequatur nulla nesciunt? Sint dolore aliquam officiis recusandae repellendus porro dolores deserunt in nobis optio nisi neque ducimus quae, nam impedit tenetur atque necessitatibus! Eos asperiores officia officiis dolor repudiandae eaque alias illum molestias vel quam inventore, animi dolorem eum culpa veritatis! Nostrum provident dignissimos tempore repellat nemo nulla soluta fugit fugiat possimus? Corrupti ut voluptates quasi odio voluptatum deleniti laboriosam vitae, labore laudantium minima doloremque accusamus vero, earum tempora nobis dolores rem, velit modi. Officia magnam adipisci, ipsam ex cum iusto numquam cumque porro eum sint quibusdam blanditiis dignissimos repudiandae nisi! Aliquam laborum at illum voluptatem ut, quis, sed sunt dignissimos provident natus amet debitis veniam beatae ducimus dicta cumque nulla nihil tenetur? Et provident veritatis laboriosam, alias temporibus eum minima totam porro nostrum ducimus repellendus quia incidunt odio, ad laborum, similique deleniti excepturi at beatae sed ipsum ratione quibusdam accusantium! Id est fugiat modi maiores quisquam sapiente esse sunt enim incidunt reprehenderit dolor recusandae facere, in cum consectetur, numquam ab. Vero reiciendis repellat accusantium tempora dolores ipsa at cum quo nam odit ducimus perspiciatis saepe harum inventore dicta soluta eos magni perferendis dolor laudantium culpa nisi ipsum, repudiandae aliquam. Iure ut officiis atque, illum sapiente officia labore ipsam, repellendus quisquam numquam quaerat culpa earum. Numquam, eveniet earum harum quos amet quia placeat eum cumque? Debitis expedita odit dicta velit est nemo impedit quisquam praesentium quia porro.</h1>
+      </div>
+    </>
+    
   )
 }
 
