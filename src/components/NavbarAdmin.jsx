ecd6384d7dc90ceaa428ab7a9ac500757d52f2bd
Mel <57514727+Mel1207@users.noreply.github.com>
2024-09-23 18:00:43 +0800

    added admin page

--- src/components/NavbarAdmin.jsx
+++ src/components/NavbarAdmin.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import BurgerMenu from '../assets/icon-burger-menu.svg'
+import IconLogOut from '../assets/icon-logout.svg'
 
 const NavbarAdmin = () => {
   return (
-    <div className='navbar-admin'>NavbarAdmin</div>
+    <div className='navbar-admin'>
+      <div className="navbar-admin-container">
+        <div className='navbar-left'>
+          <button>
+            <img src={BurgerMenu} alt="Burger menu" />
+          </button>
+          <p className='greetings'>Hello John 👋</p>
+        </div>
+        
+        <div className='account'>
+          <div className='avatar'>J</div>
+          <div className='account-details'>
+            <p className='account-name'>John Doe</p>
+            <span className='account-role'>Admin</span>
+          </div>
+          <img src={IconLogOut} alt="logout" className='logout'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
