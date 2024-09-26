c153da8aa19e9c651ab17c0cbbcbfe689da561d2
Mel <57514727+Mel1207@users.noreply.github.com>
2024-09-26 23:34:24 +0800

    added post request

--- src/hooks/useAddJobPost.js
+++ src/hooks/useAddJobPost.js
@@ -1,18 +1,36 @@
-import { addDoc, collection } from "firebase/firestore"
-import { db } from "../config/firebase-config"
+// import { addDoc, collection } from "firebase/firestore"
+// import { db } from "../config/firebase-config"
 
 export const useAddJobPost = () => {
-  const jobCollectionRef = collection(db, 'jobpost')
+  // const jobCollectionRef = collection(db, 'jobpost')
 
-  const addJobPost = async ({ jobTitle, category, type, location, setup, applicationLink, description}) => {
-    await addDoc(jobCollectionRef, {
-      jobTitle,
-      category,
-      type,
-      location,
-      setup,
-      applicationLink,
-      description
+  // const addJobPost = async ({ jobTitle, category, type, location, setup, applicationLink, description}) => {
+  //   await addDoc(jobCollectionRef, {
+  //     jobTitle,
+  //     category,
+  //     type,
+  //     location,
+  //     setup,
+  //     applicationLink,
+  //     description
+  //   })
+  // }
+
+  const addJobPost = ({ jobTitle, category, type, location, setup, applicationLink, description}) => {
+    fetch('http://localhost:8000/jobPost', {
+      method: 'POST',
+      headers: { "Content-type": "Application/json" },
+      body: JSON.stringify({
+        jobTitle,
+        category,
+        type,
+        location,
+        setup,
+        applicationLink,
+        description
+      })
+    }).then(() => {
+      console.log('new blog added')
     })
   }
 
