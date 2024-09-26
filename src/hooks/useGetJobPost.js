796e50de877c17620b67196efd99827da993feb5
Mel <57514727+Mel1207@users.noreply.github.com>
2024-09-26 21:54:14 +0800

    added mock data and buttonLink component

--- src/hooks/useGetJobPost.js
+++ src/hooks/useGetJobPost.js
@@ -1,14 +1,22 @@
-import { collection, getDocs } from "firebase/firestore"
+// import { collection, getDocs } from "firebase/firestore"
 import { useState } from "react"
-import { db } from "../config/firebase-config"
+// import { db } from "../config/firebase-config"
 
 export const useGetJobPost = () => {
   const [jobPost, setJobPost] = useState([])
-  const jobCollectionRef = collection(db, 'jobpost')
+  // const jobCollectionRef = collection(db, 'jobpost')
 
   const getJobPost = async () => {
-    const data = await getDocs(jobCollectionRef)
-    setJobPost(data.docs.map(item => ({...item.data(), id: item.id})))
+    // const data = await getDocs(jobCollectionRef)
+    // setJobPost(data.docs.map(item => ({...item.data(), id: item.id})))
+
+    try {
+      const res = await fetch('http://localhost:8000/jobPost')
+      const data = await res.json()
+      setJobPost(data)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   getJobPost()
